0a826ef8cf3e15e9b00e6ce8e6322bc6b2307a8b
Do Tuan Huy <dotuanhuy1302@gmail.com>
2022-07-12 13:47:57 +0700

    fix ui

--- src/components/CategoryHeadline/index.jsx
+++ src/components/CategoryHeadline/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Button from "../Button";
 import "./style.scss";
+import Picture1 from "../../assets/img/product_space_for_heading.png"
 import SpaceForHeading from "./spaceHeading";
+import SpaceForHeading2 from "../../assets/img/product_space_for_heading_2.png"
 export const CategoriHeadline = () => {
   return (
     <div className="categoriHeadline">
-     <SpaceForHeading title="Banner subfocus" des="Space for heading" />
-     <SpaceForHeading title="Banner subfocus 2" des="Space for heading 2"/>
+     <SpaceForHeading title="Banner subfocus" des="Space for heading" imgs={Picture1}/>
+     <SpaceForHeading title="Banner subfocus 2" des="Space for heading 2" imgs={SpaceForHeading2}/>
     </div>
   );
 };
